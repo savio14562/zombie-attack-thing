7b75fafebd880cc8d46568101568c0e955ef44dc
savio14562 <99622252+savio14562@users.noreply.github.com>
2022-07-12 17:18:29 -0400

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,3 @@
+-- Loadstring
+loadstring(game:HttpGet("https://raw.githubusercontent.com/savio14562/zombie-attack-thing/main/ESP.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/savio14562/zombie-attack-thing/main/FOVCircle.lua"))()
